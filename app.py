bfd8f4af8aeee3201c7a578107660fd817a060bc
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2023-05-06 17:29:14 +0200

    Debugging Flask API at Render.com v17

--- app.py
+++ app.py
@@ -50,9 +50,10 @@ def index():
                 "prob": pred_label_prob.tolist()}
     print(f"Response: {time.time() - start_time}")
     response = flask.Response(
-        response=response_body, status=200, mimetype="text/plain"
+        response=json.dumps(response_body), status=200, mimetype="text/plain"
     )
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
     return response
 
 
