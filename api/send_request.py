7dd658e8cd0cca36213e7a581d2f533becc61ae7
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2023-05-06 14:14:48 +0200

    Debugging Flask API at Render.com v13

--- api/send_request.py
+++ api/send_request.py
@@ -1,14 +1,13 @@
 import requests
+import time
 
 # res = requests.post('https://nlp-assignment-3.onrender.com/test')
 # print('response from server:', res.text)
 
-# dictToSend = {"text": "This majestic predator hunts for prey on the plains of Africa."}
-# res = requests.post('http://127.0.0.1:5000', json=dictToSend)
-# print('response from server:', res.text)
-# dictFromServer = res.json()
-
-dictToSend = {"text": "This majestic predator hunts for prey on the plains of Africa."}
+start_time = time.time()
+dictToSend = {"text": "This is a red, round fruit."}
 res = requests.post('https://nlp-assignment-3.onrender.com/predict', json=dictToSend)
 print('response from server:', res.text)
-# dictFromServer = res.json()
+end_time = time.time()
+
+print(end_time - start_time)
