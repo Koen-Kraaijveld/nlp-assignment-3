1ee2edb7ad4f0dc80f7f673248ddb9313883c2ee
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2023-05-09 02:13:45 +0200

    Added new model with 100 classes

--- models/LSTM.py
+++ models/LSTM.py
@@ -10,7 +10,7 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, TextVectorization, Bidirectional
+from tensorflow.keras.layers import Dense, TextVectorization, Bidirectional, Dropout
 from tensorflow.keras.layers import LSTM as LSTMLayer
 from tensorflow.keras.optimizers import Adam
 # from tensorflow.keras.saving import load_model
@@ -51,18 +51,23 @@ class LSTM:
 
         model = Sequential()
         model.add(Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=100, trainable=False))
-        model.add(LSTMLayer(128, dropout=0.5, recurrent_regularizer='l2', stateful=False))
-        # model.add(GlobalMaxPooling1D())
-        model.add(Dense(128, activation='relu'))
-        model.add(Dense(128, activation='relu'))
-        model.add(Dense(128, activation='relu'))
+        model.add(LSTMLayer(256, return_sequences=True))
+        model.add(GlobalMaxPooling1D())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.7))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.7))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.7))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.7))
         model.add(Dense(100, activation='softmax'))
 
         optimizer = Adam(learning_rate=0.001)
         model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=["acc"])
         print(model.summary())
 
-        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
+        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=15)
         model.fit(np.array(train_seq), np.array(self.__train["label"]), batch_size=64,
                   validation_split=0.2, epochs=1000, verbose=1, callbacks=[es])
         model.save("./models/saved/lstm.h5", save_format="h5")
