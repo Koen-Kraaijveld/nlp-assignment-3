9229a3c09cab5b928e4736b78c2926e9b80b9c90
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2023-06-03 02:39:38 +0200

    Ready for deployment v12

--- web/descraibeit/descraibeit/views.py
+++ web/descraibeit/descraibeit/views.py
@@ -12,12 +12,20 @@ from tensorflow import keras
 from sklearn.preprocessing import LabelEncoder
 
 
-model = keras.models.load_model("../models/saved/lstm-small.h5")
-with open('./models/saved/tokenizer.json') as f:
-    data = json.load(f)
-    tokenizer = keras_preprocessing.text.tokenizer_from_json(data)
-label_encoder = LabelEncoder()
-label_encoder.classes_ = np.load("./models/saved/labels.npy", allow_pickle=True)
+try:
+    model = keras.models.load_model("./models/saved/lstm-small.h5")
+    with open('./models/saved/tokenizer.json') as f:
+        data = json.load(f)
+        tokenizer = keras_preprocessing.text.tokenizer_from_json(data)
+    label_encoder = LabelEncoder()
+    label_encoder.classes_ = np.load("./models/saved/labels.npy", allow_pickle=True)
+except IOError:
+    model = keras.models.load_model("./saved/lstm-small.h5")
+    with open('../saved/tokenizer.json') as f:
+        data = json.load(f)
+        tokenizer = keras_preprocessing.text.tokenizer_from_json(data)
+    label_encoder = LabelEncoder()
+    label_encoder.classes_ = np.load("../saved/labels.npy", allow_pickle=True)
 
 
 def index(request):
