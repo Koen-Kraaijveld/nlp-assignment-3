db6f61759ee2db7aaa2f66f14773c0457b248aaf
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2023-06-03 02:46:20 +0200

    Ready for deployment v13

--- web/descraibeit/descraibeit/views.py
+++ web/descraibeit/descraibeit/views.py
@@ -12,21 +12,6 @@ from tensorflow import keras
 from sklearn.preprocessing import LabelEncoder
 
 
-try:
-    model = keras.models.load_model("./models/saved/lstm-small.h5")
-    with open('./models/saved/tokenizer.json') as f:
-        data = json.load(f)
-        tokenizer = keras_preprocessing.text.tokenizer_from_json(data)
-    label_encoder = LabelEncoder()
-    label_encoder.classes_ = np.load("./models/saved/labels.npy", allow_pickle=True)
-except IOError:
-    model = keras.models.load_model("./saved/lstm-small.h5")
-    with open('../saved/tokenizer.json') as f:
-        data = json.load(f)
-        tokenizer = keras_preprocessing.text.tokenizer_from_json(data)
-    label_encoder = LabelEncoder()
-    label_encoder.classes_ = np.load("../saved/labels.npy", allow_pickle=True)
-
 
 def index(request):
     context = {
@@ -37,6 +22,21 @@ def index(request):
     }
 
     if request.GET.get("btnSubmit"):
+        try:
+            model = keras.models.load_model("./models/saved/lstm-small.h5")
+            with open('./models/saved/tokenizer.json') as f:
+                data = json.load(f)
+                tokenizer = keras_preprocessing.text.tokenizer_from_json(data)
+            label_encoder = LabelEncoder()
+            label_encoder.classes_ = np.load("./models/saved/labels.npy", allow_pickle=True)
+        except IOError:
+            model = keras.models.load_model("./saved/lstm-small.h5")
+            with open('./saved/tokenizer.json') as f:
+                data = json.load(f)
+                tokenizer = keras_preprocessing.text.tokenizer_from_json(data)
+            label_encoder = LabelEncoder()
+            label_encoder.classes_ = np.load("./saved/labels.npy", allow_pickle=True)
+
         description = request.GET.get("txtDescription")
         text = [clean_text(description)]
         text = tokenizer.texts_to_sequences(text)
